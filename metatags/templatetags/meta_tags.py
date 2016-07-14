489bb3c1aeb8e31827b602d029f49ada205673f8
avalas <aastapp@gmail.com>
2016-07-14 22:57:26 +0800

    Removed title auto generation

--- metatags/templatetags/meta_tags.py
+++ metatags/templatetags/meta_tags.py
@@ -9,10 +9,6 @@ from metatags.utils import truncate_language_code
 register = Library()
 
 
-def _get_page_title(page_object, page_title_field):
-    return getattr(page_object, page_title_field, force_text(page_object))
-
-
 def _get_object_or_none(cls, *args, **kwargs):
     queryset = _get_queryset(cls)
     try:
@@ -37,13 +33,12 @@ def include_meta_tags(context, page_object=None, page_title_field='title',
                      content_type__app_label=page_object._meta.app_label,
                      content_type__model=ct_model_name)
             # Get not blank title
-            meta_tags.title = meta_tags.title or \
-                _get_page_title(page_object, page_title_field)
+            meta_tags.title = meta_tags.title or default_title
             meta_tags.keywords = meta_tags.keywords or default_keywords
             meta_tags.description = meta_tags.description or default_description
         except MetaTag.DoesNotExist:
             meta_tags = {
-                'title': _get_page_title(page_object, page_title_field),
+                'title': default_title,
                 'keywords': default_keywords,
                 'description': default_description
             }
